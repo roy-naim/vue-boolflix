05ec14756bb85850204c8fd6cf2d1474f4139024
Roy <b.roynaim@gmail.com>
2021-04-12 18:34:03 +0200

    milestone 1 completato

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+Vue.config.devtools = true;
+
 var app = new Vue(
   {
   el: '#root',
   data: {
-
+    inputSearch: '',
+    risultati: [],
   },
 
-  // mounted() {}
-  //
+  methods: {
+    search: function () {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2b27e667a5b6f8ecd1719f3f32e10b50&query=${this.inputSearch}&language=it`)
+        .then((response) =>{
+          // console.log(response.data.results);
+          this.risultati = response.data.results;
+        })
+    }
   }
+
+}
+
 );
