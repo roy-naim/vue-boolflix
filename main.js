9879ae5a41b97514eda840373e4cd8d66e0d7876
Roy <b.roynaim@gmail.com>
2021-04-13 17:00:04 +0200

    milestone 2 aggiunta tv

--- main.js
+++ main.js
@@ -6,18 +6,55 @@ var app = new Vue(
   data: {
     inputSearch: '',
     risultati: [],
+    api_key: '2b27e667a5b6f8ecd1719f3f32e10b50',
+    language: 'it',
+    uri: 'https://api.themoviedb.org/3',
   },
 
   methods: {
     search: function () {
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=2b27e667a5b6f8ecd1719f3f32e10b50&query=${this.inputSearch}&language=it`)
+      axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.inputSearch}&language=${this.language}`)
         .then((response) =>{
           // console.log(response.data.results);
-          this.risultati = response.data.results;
+          this.risultati = [...this.risultati, ...response.data.results];
         })
-    }
+      axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.inputSearch}&language=${this.language}`)
+        .then((response) =>{
+          // console.log(response.data.results);
+          this.risultati = [...this.risultati, ...response.data.results];
+        })
+    },
+
+    getTitle: function (obj) {
+      if (obj.title) {
+        return obj.title
+      } else {
+        return obj.name
+      }
+    },
+
+    getOriginalTitle: function (obj) {
+      if (obj.original_title) {
+        return obj.original_title
+      } else {
+        return obj.original_name
+      }
+    },
+
+    getOriginalLanguage: function (obj) {
+      if (obj.original_language) {
+        return obj.original_language
+      } else {
+        return obj.original_language
+      }
+    },
+
+    getVoteAverage: function (obj) {
+      if (obj.vote_average) {
+        return obj.vote_average
+      } else {
+        return obj.vote_average
+      }
+    },
   }
-
-}
-
-);
+});
